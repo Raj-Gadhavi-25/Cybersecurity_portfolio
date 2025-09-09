8f9eb39e97352d634ea5c0487ac8f5038fa8c86d
Raj Gadhavi <rajgadhavi0125@gmail.com>
2025-09-09 12:53:34 +0530

    chore: add Vercel Speed Insights

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { SpeedInsights } from '@vercel/speed-insights/react'  // ✅ import here
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    {/* Only run in production */}
+    {import.meta.env.PROD && <SpeedInsights />}
   </StrictMode>,
 )
